61fca3caf0f9bc2b17ab4c4801419a469acd1d56
Tan Nguyen <crazytan94@gmail.com>
2020-02-28 00:22:09 +0700

    Add project files.

---
+++ XAFApp/XAFApp/Views/Core/BaseContentPage.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace XAF.App.Views.Core
+{
+    public class BaseContentPage : ContentPage
+    {
+        public void SendAppearing()
+        {
+            OnAppearing();
+        }
+
+        public void SendDisappearing()
+        {
+            OnDisappearing();
+        }
+    }
+}
